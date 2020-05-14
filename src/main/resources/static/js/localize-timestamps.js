dec18b3beb988b9989e35f11c4d6adffa6eae432
Marek Suchánek <marek.suchanek@gmail.com>
2020-05-14 14:32:29 +0200

    Simplify date in JS

--- src/main/resources/static/js/localize-timestamps.js
+++ src/main/resources/static/js/localize-timestamps.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(() => {
     jQuery("td.timestamp").each(function() {
-        const dt = new Date(Date.parse($(this).text()));
+        const dt = new Date($(this).text());
         $(this).text(dt.toLocaleString("en-GB"));
     });
 });
